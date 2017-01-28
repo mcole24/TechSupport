ebc92372a35e2bc11bdd6b7a10f84dc428447e36
Matthew Cole <mattaecole@gmail.com>
2017-01-28 14:27:49 -0500

    something is attempting to be displayed in the list view but window isn't setup correctly

--- techsupport/View/OpenIncidentsForm.Designer.cs
+++ techsupport/View/OpenIncidentsForm.Designer.cs
@@ -28,20 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.openIncidentsListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // openIncidentsListView
+            // 
+            this.openIncidentsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.openIncidentsListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openIncidentsListView.Location = new System.Drawing.Point(0, 0);
+            this.openIncidentsListView.Name = "openIncidentsListView";
+            this.openIncidentsListView.Size = new System.Drawing.Size(557, 341);
+            this.openIncidentsListView.TabIndex = 0;
+            this.openIncidentsListView.UseCompatibleStateImageBehavior = false;
+            this.openIncidentsListView.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Product Code";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Date Opened";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Customer";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Technician";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Title";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // OpenIncidentsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(557, 341);
+            this.Controls.Add(this.openIncidentsListView);
             this.Name = "OpenIncidentsForm";
             this.Text = "Open Incidents";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.OpenIncidentsForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView openIncidentsListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
     }
 }
--- techsupport/View/OpenIncidentsForm.cs
+++ techsupport/View/OpenIncidentsForm.cs
@@ -7,14 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TechSupport.Controller;
+using TechSupport.Model;
+using System.Data.SqlClient;
 
 namespace TechSupport.View
 {
     public partial class OpenIncidentsForm : Form
     {
+        private IncidentsController iController;
         public OpenIncidentsForm()
         {
             InitializeComponent();
+            iController = new IncidentsController();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenIncidentsForm_Load(object sender, EventArgs e)
+        {
+            openIncidentsListView.Items.Clear();
+            List<Incidents> incidentList;
+
+            try
+            {
+                incidentList = iController.DisplayOpenIncidents();
+                if (incidentList.Count > 0)
+                {
+                    Incidents incident;
+                    for (int item = 0; item < incidentList.Count; item++)
+                    {
+                        incident = incidentList[item];
+                        openIncidentsListView.Items.Add(incident.getProductCode());
+                        openIncidentsListView.Items[item].SubItems.Add(incident.getDateOpened().ToShortDateString());
+                        openIncidentsListView.Items[item].SubItems.Add(incident.getCustomerName());
+                        openIncidentsListView.Items[item].SubItems.Add(incident.getTechnician());
+                        openIncidentsListView.Items[item].SubItems.Add(incident.getTitle());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No open incidents were found.");
+                    this.Close();
+                }
+            }
+            catch (SqlException excep) {
+                MessageBox.Show(excep.Message, excep.GetType().ToString());
+                this.BeginInvoke(new MethodInvoker(Close));
+            }
+
         }
     }
 }
