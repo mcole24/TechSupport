5cedbf56a3f970cee999d1a820cf9c84ad68490d
Matthew Cole <mattaecole@gmail.com>
2017-01-28 12:02:43 -0500

    setup MVC structure and created DBConnection class

---
+++ TechSupport/DBAccess/DBConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TechSupport.DBAccess {
+
+    //Sets up the database connection
+    class DBConnection {
+
+        public static SqlConnection GetConnection() {
+            try
+            {
+                string connectionString = "Data Source=localhost;Initial Catalog=TechSupport;Integrated Security=True;";
+                SqlConnection connection = new SqlConnection(connectionString);
+                return connection;
+            }
+            catch (SqlException exception) {
+                throw exception;
+            }
+        }
+
+    }
+}
