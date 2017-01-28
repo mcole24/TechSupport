d3f7b959eac0810625a11e4f048ff4f178ce5088
Matthew Cole <mattaecole@gmail.com>
2017-01-28 12:13:35 -0500

    created empty class files in DBAccess, Model, and Controller to handle Incidents data

---
+++ TechSupport/Controller/IncidentsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechSupport.DBAccess;
+
+namespace TechSupport.Controller
+{
+    class IncidentsController
+    {
+
+        public IncidentsController() { }
+
+        
+
+    }
+}
