12aeb32d597c0a8885f4e2a60dc49cdce4622151
Matthew Cole <mattaecole@gmail.com>
2017-02-05 18:59:54 -0500

    Changed Incidents.cs to use properties and removed private variables.

--- TechSupport/Model/Incidents.cs
+++ TechSupport/Model/Incidents.cs
@@ -9,77 +9,27 @@ namespace TechSupport.Model
     class Incidents
     {
 
-        private int incidentID;
-        private int customerID;
-        private string productCode;
-        private int techID;
-        private DateTime dateOpened;
-        private DateTime dateClosed;
-        private string title;
-        private string description;
-        private string customerName;
-        private string technician;
-
         public Incidents() { }
 
-        public int IncidentID {
-            get { return incidentID; }
-            set { incidentID = value; }
-        }
+        public int IncidentID { get; set; }
 
-        public int CustomerID
-        {
-            get { return customerID; }
-            set { customerID = value; }
-        }
+        public int CustomerID { get; set; }
 
-        public string ProductCode
-        {
-            get { return productCode; }
-            set { productCode = value; }
-        }
+        public string ProductCode { get; set; }
 
-        public int TechID
-        {
-            get { return techID; }
-            set { techID = value; }
-        }
+        public int TechID { get; set; }
 
-        public DateTime DateOpened
-        {
-            get { return dateOpened; }
-            set { dateOpened = value; }
-        }
+        public DateTime DateOpened { get; set; }
 
-        public DateTime DateClosed
-        {
-            get { return dateClosed; }
-            set { dateClosed = value; }
-        }
+        public DateTime DateClosed { get; set; }
 
-        public string Title
-        {
-            get { return title; }
-            set { title = value; }
-        }
+        public string Title { get; set; }
 
-        public string Description
-        {
-            get { return description; }
-            set { description = value; }
-        }
+        public string Description { get; set; }
 
-        public string CustomerName
-        {
-            get { return customerName; }
-            set { customerName = value; }
-        }
+        public string CustomerName { get; set; }
 
-        public string Technician
-        {
-            get { return technician; }
-            set { technician = value; }
-        }
+        public string Technician { get; set; }
 
     }
 }
