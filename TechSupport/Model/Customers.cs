3d96f77ab6a9cceeabaad0427102b3ea25206ca7
Matthew Cole <mattaecole@gmail.com>
2017-02-06 18:13:46 -0500

    Created Model classes for Customers, Products, and Technicians

---
+++ TechSupport/Model/Customers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.Model
+{
+    class Customers
+    {
+
+        public Customers() { }
+
+        public int CustomerID { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
