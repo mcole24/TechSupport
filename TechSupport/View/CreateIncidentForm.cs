518c2b320723639ea9751939e104e9c294bc5ec4
Matthew Cole <mattaecole@gmail.com>
2017-02-06 19:01:43 -0500

    implemented the CreateIncidentForm_Load in the CreateIncidentForm class

--- TechSupport/View/CreateIncidentForm.cs
+++ TechSupport/View/CreateIncidentForm.cs
@@ -9,14 +9,23 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using TechSupport.Model;
 using TechSupport.Controller;
+using System.Data.SqlClient;
 
 namespace TechSupport.View
 {
     public partial class CreateIncidentForm : Form
     {
+
+        private CustomersController cControl;
+        private ProductsController pControl;
+        private TechniciansController tControl;
+
         public CreateIncidentForm()
         {
             InitializeComponent();
+            cControl = new CustomersController();
+            pControl = new ProductsController();
+            tControl = new TechniciansController();
         }
 
         private void createIncidentButton_Click(object sender, EventArgs e)
@@ -31,7 +40,22 @@ namespace TechSupport.View
 
         private void CreateIncidentForm_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                customerNameBox.DataSource = this.cControl.GetCustomers();
+                customerNameBox.DisplayMember = "Name";
+                customerNameBox.ValueMember = "CustomerID";
+                customerNameBox.SelectedIndex = -1;
+                productNameBox.DataSource = this.pControl.GetProducts();
+                productNameBox.DisplayMember = "ProductCode";
+                productNameBox.ValueMember = "ProductCode";
+                productNameBox.SelectedIndex = -1;
+            }
+            catch (SqlException sqlex)
+            {
+                MessageBox.Show(sqlex.Message, sqlex.GetType().ToString());
+                this.BeginInvoke(new MethodInvoker(Close));
+            }
         }
     }
 }
