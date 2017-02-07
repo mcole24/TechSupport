e6833e7c63dddd91a97810eaf980f227d8f730ca
Matthew Cole <mattaecole@gmail.com>
2017-02-06 19:12:47 -0500

    implemented more CreateIncidentForm.cs functionality. I need to go back and add functionality to Incidents controller.

--- TechSupport/View/CreateIncidentForm.cs
+++ TechSupport/View/CreateIncidentForm.cs
@@ -19,6 +19,7 @@ namespace TechSupport.View
         private CustomersController cControl;
         private ProductsController pControl;
         private TechniciansController tControl;
+        private IncidentsController iControl;
 
         public CreateIncidentForm()
         {
@@ -26,16 +27,43 @@ namespace TechSupport.View
             cControl = new CustomersController();
             pControl = new ProductsController();
             tControl = new TechniciansController();
+            iControl = new IncidentsController();
         }
 
         private void createIncidentButton_Click(object sender, EventArgs e)
         {
+            if (titleBox.Text == "")
+            {
+                MessageBox.Show("Title cannot be blank.");
+            }
+            else if (descriptionBox.Text == "")
+            {
+                MessageBox.Show("Description cannot be blank.");
+            }
+            else
+            {
 
+                Incidents inc = new Incidents();
+                inc.CustomerID = (int)customerNameBox.SelectedValue;
+                inc.ProductCode = productNameBox.SelectedValue.ToString();
+                inc.Title = titleBox.Text;
+                inc.Description = descriptionBox.Text;
+
+                try
+                {
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.GetType().ToString());
+                    this.BeginInvoke(new MethodInvoker(Close));
+                }
+            }
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void CreateIncidentForm_Load(object sender, EventArgs e)
