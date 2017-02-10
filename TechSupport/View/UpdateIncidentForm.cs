0f5f2c14f5f71c84677455027181c10b278bb959
Matthew Cole <mattaecole@gmail.com>
2017-02-10 16:19:28 -0500

    designed layout for update incident form

---
+++ TechSupport/View/UpdateIncidentForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechSupport.View
+{
+    public partial class UpdateIncidentForm : Form
+    {
+        public UpdateIncidentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateIncidentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
